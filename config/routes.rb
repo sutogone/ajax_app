da14b2c83601352053c97f3d02a389b631460170
sutogone <batorukunn@yahoo.co.jp>
2020-10-22 04:53:29 +0900

    メモ投稿機能のフロントエンド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  post 'posts', to: 'posts#create'
-  get 'posts/:id', to: 'posts#checked'
+  post 'posts', to: 'posts#create'      #メモ投稿機能(Ajax)
+  get 'posts/:id', to: 'posts#checked'     #既読機能(Ajax)
 end
